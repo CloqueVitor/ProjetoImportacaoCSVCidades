3131423e731aa2fee7cc5384422cbd51b3504488
vitor <vitor@10.48.3.119>
2017-01-12 20:44:08 -0200

    - Adicionado documento com instruções de execução do projeto  no local - (src/main/resources/instrucoes); cobertura de 100% dos testes;

--- src/test/java/br/sc/vitor/ProcessaEntradaTeste.java
+++ src/test/java/br/sc/vitor/ProcessaEntradaTeste.java
@@ -24,11 +24,76 @@ public class ProcessaEntradaTeste {
 	}
 	
 	@Test
+	public void quandoEntrarComCoutDistinctUFdeveRetornarOtotalDeItensDistintos() throws Exception {
+
+		ProcessaEntrada processaEntrada = new ProcessaEntrada();
+		int total = processaEntrada.processar("count distinct UF");
+		Assert.assertEquals(0, total);
+	}
+	
+	@Test
 	public void quandoEntrarComCoutDistinctNameDeveRetornarOtotalDeItensDistintos() throws Exception {
 
 		ProcessaEntrada processaEntrada = new ProcessaEntrada();
 		int total = processaEntrada.processar("count distinct name");
 		Assert.assertEquals(5291, total);
-
 	}
+	
+
+	@Test
+	public void quandoEntrarComCoutDistinctCapitalDeveRetornarOtotalDeItensDistintos() throws Exception {
+
+		ProcessaEntrada processaEntrada = new ProcessaEntrada();
+		int total = processaEntrada.processar("count distinct capital");
+		Assert.assertEquals(2, total);
+	}
+	
+	@Test
+	public void quandoEntrarComCoutDistinctLonDeveRetornarOtotalDeItensDistintos() throws Exception {
+
+		ProcessaEntrada processaEntrada = new ProcessaEntrada();
+		int total = processaEntrada.processar("count distinct lon");
+		Assert.assertEquals(5565, total);
+	}
+	
+	@Test
+	public void quandoEntrarComCoutDistinctLatDeveRetornarOtotalDeItensDistintos() throws Exception {
+
+		ProcessaEntrada processaEntrada = new ProcessaEntrada();
+		int total = processaEntrada.processar("count distinct lat");
+		Assert.assertEquals(5565, total);
+	}
+	
+	@Test
+	public void quandoEntrarComCoutDistinctNo_accentsDeveRetornarOtotalDeItensDistintos() throws Exception {
+
+		ProcessaEntrada processaEntrada = new ProcessaEntrada();
+		int total = processaEntrada.processar("count distinct no_accents");
+		Assert.assertEquals(0, total);
+	}
+	
+	@Test
+	public void quandoEntrarComCoutDistinctAlternative_namesDeveRetornarOtotalDeItensDistintos() throws Exception {
+
+		ProcessaEntrada processaEntrada = new ProcessaEntrada();
+		int total = processaEntrada.processar("count distinct alternative_names");
+		Assert.assertEquals(0, total);
+	}
+	
+	@Test
+	public void quandoEntrarComCoutDistinctMicroregionDeveRetornarOtotalDeItensDistintos() throws Exception {
+
+		ProcessaEntrada processaEntrada = new ProcessaEntrada();
+		int total = processaEntrada.processar("count distinct microregion");
+		Assert.assertEquals(0, total);
+	}
+	
+	@Test
+	public void quandoEntrarComCoutDistinctMesoregionDeveRetornarOtotalDeItensDistintos() throws Exception {
+
+		ProcessaEntrada processaEntrada = new ProcessaEntrada();
+		int total = processaEntrada.processar("count distinct mesoregion");
+		Assert.assertEquals(0, total);
+	}
+	
 }
